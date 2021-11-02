8a0d148fefaa6e1c1d9fa9f0004716a6dbeeea19
Hyunjin Cho <jhj176610@gmail.com>
2021-11-02 16:02:04 -0500

    Add SearchActivity

--- app/src/main/java/com/example/demo_second/SearchMovies.java
+++ app/src/main/java/com/example/demo_second/SearchMovies.java
@@ -1,75 +1,55 @@
 package com.example.demo_second;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchMovies extends AppCompatActivity {
 
-    // 1 - Link for popular movie
-    private static String movie_db_url = "https://api.themoviedb.org/3/movie/popular?api_key=a7dab8c476fb31214a42e7867fa023b2";
+    EditText et_search;
+    Button btn_search;
+    private static String movie_db_url = "https://api.themoviedb.org/3/search/movie?api_key=a7dab8c476fb31214a42e7867fa023b2&query=";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search_movies);
 
-        Toolbar myToolbar =  findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        et_search = findViewById(R.id.et_search);
+        btn_search = findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String queryString = et_search.getText().toString();
+                Log.d("55", movie_db_url + et_search.getText().toString());
 
-        ParseDBURL parseDBURL = new ParseDBURL();
-        parseDBURL.execute(movie_db_url);
+                SearchMovies.ParseDBURL parseDBURL = new SearchMovies.ParseDBURL();
+                parseDBURL.execute(movie_db_url + queryString);
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        if(item.getItemId() == R.id.menu_sesarch){
-            Intent intent = new Intent(MainActivity.this, SearchMovies.class);
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // inflate the options menu
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    private class ParseDBURL extends AsyncTask<String, String, String>{
+    private class ParseDBURL extends AsyncTask<String, String, String> {
         ArrayList<MovieModel> movieModels = new ArrayList<>();
-        ListView lv_movie = (ListView) findViewById(R.id.lv_Listview);
+        ListView lv_movie = findViewById(R.id.lv_Listview);
         String img_path_url = "https://image.tmdb.org/t/p/w500";
 
         @Override
@@ -95,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
                     current.append((char) data);
                     data = inputStreamReader.read();
                 }
-                Log.d("asdf", "doInBackground");
                 return current.toString();
 
             } catch (IOException e) {
@@ -110,30 +89,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            //Bitmap bitmap = null;
             try {
                 JSONObject obj = new JSONObject(s);
                 JSONArray movies = obj.getJSONArray("results");
 
                 for(int i=0; i< movies.length(); i++){
                     JSONObject object = movies.getJSONObject(i);
-
+                    MovieModel model = new MovieModel();
                     String title = object.getString("title");
                     String date = object.getString("release_date");
                     String img_path = img_path_url + object.getString("poster_path");
 
-                    MovieModel model = new MovieModel();
                     model.setTitle(title);
                     model.setRelease_date(date);
                     model.setImg_path(img_path);
-                    // Log.d("adf", img_path);
+
                     movieModels.add(model);
 
                 }
 
-                adapter adapter = new adapter(MainActivity.this, R.layout.movies_list, movieModels);
+                adapter adapter = new adapter(SearchMovies.this, R.layout.movies_list, movieModels);
                 lv_movie.setAdapter(adapter);
-                Log.d("adf", "adapter");
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -142,5 +118,4 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-
 }
--- app/src/main/java/com/example/demo_second/adapter.java
+++ app/src/main/java/com/example/demo_second/adapter.java
@@ -1,2 +1,59 @@
-package com.example.demo_second;public class adapter {
+package com.example.demo_second;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class adapter extends ArrayAdapter<MovieModel> {
+
+    private final Context context;
+    private final int movie_layoutID;
+
+    public adapter(@NonNull Context context, int movie_layoutID, @NonNull List<MovieModel> movies) {
+        super(context, movie_layoutID, movies);
+
+        this.context = context;
+        this.movie_layoutID = movie_layoutID;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null){
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView = layoutInflater.inflate(movie_layoutID, parent, false);
+
+        }
+
+        TextView tvTitle = convertView.findViewById(R.id.tv_title);
+        TextView tvDate = convertView.findViewById(R.id.tv_release_date);
+        ImageView ivCover = convertView.findViewById(R.id.iv_cover);
+
+        MovieModel movieModel = getItem(position);
+
+        tvTitle.setText(movieModel.getTitle());
+        tvDate.setText(movieModel.getRelease_date());
+
+        Glide.with(context).load(movieModel.getImg_path()).into(ivCover);
+
+
+        return convertView;
+    }
 }
